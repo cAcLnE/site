6e29425f6bb516743a4e08f76c981d65ac1c1032
cAcLnE <perastrol45@gmail.com>
2025-08-20 15:24:06 -0700

    Update 9.js

--- SpaceAge/9.js
+++ SpaceAge/9.js
@@ -3,14 +3,14 @@ let clickCount = 0;
 const targetClicks = 9; 
 const redirectPage = "9.html"; 
 
-const externalButton = document.getElementById("externalButton");
-const clickCount = document.getElementById("clickCount");
+const externalButton = document.getElementById('externalButton');
+const clickCountSpan = document.getElementById('clickCount');
 
-externalButton.addEventListener("click", () => {
-    clickCount++;
-    clickCount.textContent = clickCount;
+externalButton.addEventListener('click', () => {
+    count++; 
+    clickCountSpan.textContent = count;
 
-    if (clickCount >= targetClicks) {
+    if (count >= targetClicks) {
         window.location.href = 9.html;
     }
 });

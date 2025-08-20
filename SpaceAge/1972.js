3e2c1bea00e9dc4e41e3488c261e2d4d8d28b45a
cAcLnE <perastrol45@gmail.com>
2025-08-20 16:11:03 -0700

    Update 1972.js

--- SpaceAge/1972.js
+++ SpaceAge/1972.js
@@ -1,1 +1,15 @@
+let count = 0;
+const targetClicks = 1972; 
+const redirectPage = 'https://caclne.github.io/site/SpaceAge/1972.html'; 
 
+const externalButton = document.getElementById('externalButton');
+const clickCountSpan = document.getElementById('clickCount');
+
+externalButton.addEventListener('click', () => {
+    count++;
+    clickCountSpan.textContent = count;
+
+    if (count >= targetClicks) {
+        window.location.href = redirectPage;
+    }
+});
